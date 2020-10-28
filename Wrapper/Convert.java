1c4b55edff03eb0c401d09b582f5206c4655dcea
Anita-Naragund <72910010+Anita-Naragund@users.noreply.github.com>
2020-10-28 23:33:25 +0530

    My Seventh Commit

---
+++ Wrapper/Convert.java
@@ -0,0 +1,9 @@
+public class Convert
+{
+public static void main(String[] args)
+{
+	System.out.println("The String is converted into integer");
+	int value=Integer.parseInt(args[0]);
+	System.out.println("Int value is :"+value);
+}
+}
---
+++ Wrapper/LifeSpan.java
@@ -0,0 +1,12 @@
+public class LifeSpan
+{
+public static void main(String[] age)
+{
+	System.out.println("The String is converted into integer");
+	String ageOfPerson=age[0];
+	int value=Integer.parseInt(ageOfPerson);
+	String ageIs=Aayush.lifeSpan(value);
+	System.out.println("Int value is :"+ageIs);
+	System.out.println("EXIT::main()");
+}
+}
