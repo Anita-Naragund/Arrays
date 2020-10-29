9baed38815865272bde1354e026b5dc91000cd18
Anita-Naragund <72910010+Anita-Naragund@users.noreply.github.com>
2020-10-29 10:32:25 -0700

    My Eighth Commit

---
+++ Wrapper/Anu.java
@@ -0,0 +1,15 @@
+public class Anu
+{
+	public static void main(String[] anu)
+	{
+		System.out.println("Invoke main method");
+		System.out.println("The system will convert String into Number");
+		String division="anu";
+		char convertDivision=division.charAt(1);
+		System.out.println("String to char :"+convertDivision);
+		
+		
+		System.out.println("EXIT :main()");
+	}
+	
+}
---
+++ Wrapper/Convert.java
@@ -0,0 +1,9 @@
+public class Life
+{
+public static void main(String[] lifespan)
+{
+	System.out.println("The String is converted into integer");
+	int value=Integer.parseInt(args[0]);
+	System.out.println("Int value is :"+value);
+}
+}
