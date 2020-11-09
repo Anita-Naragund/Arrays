6cbf65657b61dc44a696d33ad6e7486d380b8341
Anita-Naragund <72910010+Anita-Naragund@users.noreply.github.com>
2020-11-09 19:52:14 +0530

    Updated

--- switch-enum/ENUM/InstituteTester.java
+++ switch-enum/ENUM/InstituteTester.java
@@ -3,8 +3,8 @@ public class InstituteTester
 	public static void main(String[] photos)
 	{
 		System.out.println("Invoked main doing enum ");
-		InstituteFinder.lowestAndBestestUsingString("whatever");
-		String instituteName="ELITMUS";
+		InstituteFinder.lowestAndBestestUsingString("ABC");
+		String instituteName="XWORKZ";
 		Institute enumInstituteName=Institute.valueOf(instituteName);
 		InstituteFinder.lowestAndBestestUsingEnum(enumInstituteName);
 		
