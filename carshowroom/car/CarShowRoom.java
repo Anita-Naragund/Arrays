091607c57063450163ab640e7f51aadb033bcd27
Anita-Naragund <72910010+Anita-Naragund@users.noreply.github.com>
2020-12-16 22:45:33 +0530

    Update CarShowRoom.java

--- carshowroom/car/CarShowRoom.java
+++ carshowroom/car/CarShowRoom.java
@@ -1,7 +1,11 @@
 package com.xworkz.carshowroom.car;
 
 import java.util.Arrays;
+/** 
+ * @author Anita
+ */
 
+import com.xworkz.carshowroom.carENUM.CarVarient;
 import com.xworkz.carshowroom.dto.CarDTO;
 import com.xworkz.carshowroom.dto.ManagerDTO;
 
@@ -12,17 +16,20 @@ public class CarShowRoom {
 	private CarDTO[] cars = new CarDTO[5];
 	private int currentIndex = 0;
 
+	// This is a default constructor
 	public CarShowRoom() {
 		super();
 		System.out.println("Created default Constructor");
 	}
 
+	// this is the Constructor with 2 parameters
 	public CarShowRoom(String name, long location) {
 		super();
 		this.name = name;
 		this.location = location;
 	}
 
+	// It is the method to add the manager details and it has some validation also
 	public void addManager(String name, long contactNo) {
 		if (name != null || contactNo > 0) {
 			this.managerDTO = new ManagerDTO(name, contactNo);
@@ -32,6 +39,7 @@ public class CarShowRoom {
 		}
 	}
 
+	// Display method is used for displaying the details of the manager
 	public void displayManager() {
 		if (this.managerDTO != null) {
 			System.out.println("Name is" + this.managerDTO.getName());
@@ -41,6 +49,7 @@ public class CarShowRoom {
 		}
 	}
 
+	// This method will update the manager details only contactNo is update here
 	public void updateManager(long contactNo) {
 		if (contactNo > 0) {
 			this.managerDTO.setContactNo(contactNo);
@@ -48,22 +57,74 @@ public class CarShowRoom {
 		}
 	}
 
+	// delete method will delete the details of the Manager
 	public void deleteManager() {
 		this.managerDTO = null;
 		System.out.println("Delete the values");
 	}
 
+	// This method creates the addCar method to add the cars to the Array and here I
+	// have done some validation
 	public void addCars(CarDTO dto) {
 		if (dto != null && this.currentIndex < 5) {
+
 			this.cars[currentIndex] = dto;
 			currentIndex++;
-			System.out.println("Add the cars");
+			System.out.println("Current Index" + this.currentIndex);
 
 		} else {
-
-			System.out.println("Cannot add " + this.cars);
+			if (dto == null) {
+				System.out.println("We can not an empty or null");
+			}
+			if (dto != null) {
+				System.out.println("Cannot add more cars,total " + this.currentIndex);
+			}
+			System.out.println("Current index :" + this.currentIndex);
 		}
 	}
+
+	// displayCar() method is used to display the car details that are stored in an
+	// array
+	public void displayCar() {
+		CarDTO[] tempCars = this.cars;
+		for (int i = 0; i < tempCars.length; i++) {
+			CarDTO dto = tempCars[i];
+			System.out.println(dto);
+			System.out.println(dto.getModel());
+			System.out.println(dto.getPrice());
+			System.out.println(dto.getVarient());
+		}
 	}
 
+	public void updateCarVarient(CarVarient newVarient, int indexToUpdate) {
+		if (newVarient != null && indexToUpdate >= 0 && indexToUpdate < 5) {
+			System.out.println("Car is at :" + indexToUpdate);
+			System.out.println(newVarient);
+			CarDTO carDTO = this.cars[indexToUpdate];
+			System.out.println("Name of car is " + carDTO.getModel());
+			carDTO.setVarient(newVarient);
+		}
+	}
 
+	public void carAtIndex(int index) {
+		if (index == 0 && index < 5) {
+			System.out.println("Index value is" + index);
+			CarDTO cars = this.cars[index];
+			System.out.println(cars.getModel());
+		}
+
+	}
+
+	public void displayByModel(String model) {
+		if (model != null) {
+			System.out.println("car name is:" + model);
+			for (int index = 0; index < this.cars.length; index++) {
+				String localModel = cars[index].getModel();
+				if (localModel.equals(model)) {
+					System.out.println(cars[index].getPrice());
+					System.out.println(cars[index].getVarient());
+				}
+			}
+		}
+	}
+}
